7fbdcca8c7b73db8769bac1c7c480b0afe426adf
P.C <pilamb@users.noreply.github.com>
2019-04-09 18:31:14 +0200

    Tdd2 models (#4)
    * App Players enabled, views, serializers, urls and settings enabled to use Django User
    
    * some tests covering user serializer and viewset
    
    * forgotten PEP8

--- mastermindAPI/config/settings.py
+++ mastermindAPI/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'board',
-
+    'players',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
