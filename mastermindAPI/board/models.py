21f979d7f80c04d921e17059062de024e9f55fba
P.C <pilamb@users.noreply.github.com>
2019-04-09 16:57:24 +0200

    Tdd1 models (#3)
    * Creating board app, linking it to settings, and making first failing test
    
    * First Game model, first passing green test, and the SQL migrations
    
    * adding code field to Game, default value tested and its migration
    
    * Code field defailt value to a function that generates random color based on the CHOICE attr
    
    * Mock module added to Pip and new test mocking random to test default value of code
    
    * Mock module added to Pip and new test mocking random to test default value of code
    
    * Helper function to translate from letters to human readable names, its test and a Meta option to order by date
    
    * Game now has a function that check matches in color and or position returning feedback in tuple with number of pegs, and the test
    
    * Adding some business logic by ending the game is Finished changes to True, and some tests

--- mastermindAPI/board/models.py
+++ mastermindAPI/board/models.py
@@ -19,10 +19,19 @@ def create_code():
     """
     return random.choices([i for i, j in CODE_CHOICES], k=4)
 
+def decode(code):
+    """
+    Like get_code_display's Django but for a tuple.
+    Display human readable color names instead of only a letter.
+    """
+    return [name for i in code for item, name in CODE_CHOICES if item == i]
+
 
 class Game(models.Model):
     """
     Represent a board game.
+    Responsible for turn count, generating
+    the colors code, and the game main management
     """
 
     created = models.DateTimeField(auto_now_add=True)
@@ -30,5 +39,32 @@ class Game(models.Model):
     code = models.CharField(choices=CODE_CHOICES,
                             max_length=4, default=create_code)
 
+    class Meta:
+        ordering = ('created',)
+
+    @property
+    def get_code_display(self):
+        return decode(self.code)
+
+    def check_combination(self, combination):
+        return combination == self.code
+
+    def check_pegs(self, combination):
+        """
+        Count for right colors in wrong place,
+        and for right colors in the right place
+        """
+        white_pegs = 0  # but not a match in the right place
+        black_pegs = 0  # right color and in right place
+        for position, item in enumerate(combination):
+            if item.upper() in self.code and item == self.code[position]:
+                black_pegs += 1
+            elif item.upper() in self.code:
+                white_pegs += + 1
+        return white_pegs, black_pegs
+
+    def end_game(self):
+        self.finished = True
+    
     def check_combination(self, combination):
         return combination == self.code
--- mastermindAPI/board/tests.py
+++ mastermindAPI/board/tests.py
@@ -2,7 +2,7 @@ import mock
 
 from django.test import TestCase
 
-from board.models import Game, create_code
+from board.models import Game, create_code, decode
 
 
 class BoardTestCase(TestCase):
@@ -12,6 +12,8 @@ class BoardTestCase(TestCase):
 
     def test_create_game(self):
         self.assertIsNotNone(self.game)
+        self.assertIsNotNone(self.game.code)
+        self.assertIs(self.game.finished, False)
 
     def test_create_code(self):
         with mock.patch("random.choices",
@@ -27,3 +29,20 @@ class BoardTestCase(TestCase):
         game = Game.objects.create()
         game.code = ['R', 'G', 'B', 'P']
         self.assertEqual(game.check_combination(combination=combination), True)
+
+    def test_get_code_display(self):
+        self.game.code = ['R', 'G', 'B', 'P']
+        self.assertEqual(decode(self.game.code), ['RED', 'GREEN', 'BLUE', 'PURPLE'])
+
+    def test_check_pegs(self):
+        self.game.code = ['R', 'G', 'B', 'P']
+        combination1 = ['A', 'C', 'Z', 'H']
+        combination2 = ['R', 'G', 'Y', 'P']
+        combination3 = ['R', 'G', 'B', 'P']
+        self.assertEqual(self.game.check_pegs(combination1), (0,0))
+        self.assertEqual(self.game.check_pegs(combination2), (0,3))
+        self.assertEqual(self.game.check_pegs(combination3), (0,4))
+
+    def test_finish_game(self):
+        self.game.end_game()
+        self.assertIs(self.game.finished, True)
