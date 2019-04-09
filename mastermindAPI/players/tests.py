d6a7f6cd73b3c328983480670d5816d28ee64dcf
pilamb <perikopalotes@gmail.com>
2019-04-09 18:27:16 +0200

    some tests covering user serializer and viewset

--- mastermindAPI/players/tests.py
+++ mastermindAPI/players/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
 
-# Create your tests here.
+from players.serializers import UserSerializer
+from players.views import UserViewSet
+
+class GeneralUserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='test1')
+        User.objects.create(username='test2')
+
+    def test_user_serializer(self):
+        client = Client()
+        response = client.get('/players/')
+        all_users = User.objects.all()
+        serializer = UserSerializer(all_users, many=True)
+        self.assertEqual(len(response.data), len(serializer.instance))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_view(self):
+        view = UserViewSet
+        query1 = view.queryset.first()
+        query2 = User.objects.filter(is_staff=False,
+                                     is_active=True).order_by(
+                                     '-date_joined').first()
+        self.assertAlmostEqual(query1, query2)
