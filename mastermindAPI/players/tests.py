d172cea6217dc65ed796c963497e729a0f213829
pilamb <perikopalotes@gmail.com>
2019-04-09 18:27:59 +0200

    forgotten PEP8

--- mastermindAPI/players/tests.py
+++ mastermindAPI/players/tests.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from rest_framework import status
-from rest_framework.reverse import reverse
 
 from players.serializers import UserSerializer
 from players.views import UserViewSet
+
 
 class GeneralUserTestCase(TestCase):
     def setUp(self):
