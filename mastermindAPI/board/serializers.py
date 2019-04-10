f654750e06930505273786bed409ba29436a3052
pilamb <perikopalotes@gmail.com>
2019-04-10 22:11:32 +0200

    TDD5: green: Game serializer first test using it that passes

--- mastermindAPI/board/serializers.py
+++ mastermindAPI/board/serializers.py
@@ -4,4 +4,10 @@ from .models import Game, Movement
 
 
 class BoardSerializer(serializers.ModelSerializer):
-    pass
+    code = serializers.CharField(source='get_code_display')
+    player = serializers.ReadOnlyField(source='player.username')
+    finished = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Game
+        fields = ('player', 'created', 'code', 'finished')
--- mastermindAPI/board/tests/test_serializers.py
+++ mastermindAPI/board/tests/test_serializers.py
@@ -1,13 +1,17 @@
-import mock
-
 from django.test import TestCase
+from django.contrib.auth.models import User as Player
+
 from rest_framework.serializers import ValidationError
 
 from board.models import Game, Movement
-from board.serializers import Game
+from board.serializers import BoardSerializer
 
 
 class GameSerializersTestCase(TestCase):
 
-    def test_game_serializer(self):
-        pass
+    def test_board_serializer(self):
+        player = Player.objects.create()
+        game = Game.objects.create(player=player)
+        serializer = BoardSerializer(data={'code': str(game.code),
+                                           'player':player})
+        self.assertIsNotNone(serializer.instance)
