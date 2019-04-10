9cda082ba9f4417ad464dafb9d20274762fc42fc
pilamb <perikopalotes@gmail.com>
2019-04-10 20:23:53 +0200

    Some Asserts added And making import Fail since Movmenet does not exist

--- mastermindAPI/board/tests.py
+++ mastermindAPI/board/tests.py
@@ -3,7 +3,7 @@ import mock
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from board.models import Game, create_code, decode
+from board.models import Game, create_code, decode, Movement
 
 
 class BoardTestCase(TestCase):
@@ -28,8 +28,10 @@ class BoardTestCase(TestCase):
 
     def test_check_combination(self):
         combination = ['R', 'G', 'B', 'P']
+        combination2 = ['R', 'G', 'B', 'Z']
         game = Game(code = ['R', 'G', 'B', 'P'])
         self.assertEqual(game.check_combination(combination=combination), True)
+        self.assertFalse(game.check_combination(combination2))
 
     def test_get_code_display(self):
         self.game.code = ['R', 'G', 'B', 'P']
@@ -40,9 +42,11 @@ class BoardTestCase(TestCase):
         combination1 = ['A', 'C', 'Z', 'H']
         combination2 = ['R', 'G', 'Y', 'P']
         combination3 = ['R', 'G', 'B', 'P']
+        combination4 = ['A', 'Z', 'C', 'R']
         self.assertEqual(self.game.check_pegs(combination1), (0,0))
         self.assertEqual(self.game.check_pegs(combination2), (0,3))
         self.assertEqual(self.game.check_pegs(combination3), (0,4))
+        self.assertEqual(self.game.check_pegs(combination4), (1,0))
 
     def test_finish_game(self):
         self.game.end_game()
@@ -53,3 +57,4 @@ class BoardTestCase(TestCase):
 
     def test_game_owner(self):
         self.assertEqual(self.game.player.username, self.player.username)
+
