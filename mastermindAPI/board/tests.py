459c3418d932604baad3ec51b62d7bb96b1d3199
P.C <pilamb@users.noreply.github.com>
2019-04-10 21:38:12 +0200

    Develop (#9)
    * Initial Django setup: pip, requirements, and config in order to runserver
    
    * Tdd1 models (#2)
    
    * Creating board app, linking it to settings, and making first failing test
    
    * First Game model, first passing green test, and the SQL migrations
    
    * adding code field to Game, default value tested and its migration
    
    * Code field defailt value to a function that generates random color based on the CHOICE attr
    
    * Mock module added to Pip and new test mocking random to test default value of code
    
    * Mock module added to Pip and new test mocking random to test default value of code
    
    * Tdd1 models (#3)
    
    * Creating board app, linking it to settings, and making first failing test
    
    * First Game model, first passing green test, and the SQL migrations
    
    * adding code field to Game, default value tested and its migration
    
    * Code field defailt value to a function that generates random color based on the CHOICE attr
    
    * Mock module added to Pip and new test mocking random to test default value of code
    
    * Mock module added to Pip and new test mocking random to test default value of code
    
    * Helper function to translate from letters to human readable names, its test and a Meta option to order by date
    
    * Game now has a function that check matches in color and or position returning feedback in tuple with number of pegs, and the test
    
    * Adding some business logic by ending the game is Finished changes to True, and some tests
    
    * Tdd2 models (#4)
    
    * App Players enabled, views, serializers, urls and settings enabled to use Django User
    
    * some tests covering user serializer and viewset
    
    * forgotten PEP8
    
    * Tdd3 models (#5)
    
    * TDD failing test expecting a user - adding player as a game owner and its migration
    
    * upgrading model tests to have Player on them
    
    * Coverage added to requirements
    
    * Makefile with many shortcuts: coverage, clean-pyc, clean-report, test... README with instructions to use it and requirementes updated with tools (#6)
    
    * Tdd4 models red green refactor iteration (#8)
    
    * Some Asserts added And making import Fail since Movmenet does not exist
    
    * Gitignore updated
    
    * TDD: adding Model Movement since tests fails importing it
    
    * TDD: gree: Movement creation OK
    
    * TDD: red: Movement does not have a Game linked to it
    
    * TDD: red: Movement Game migrations and model field added
    
    * TDD5: green: test refactor with new field
    
    * TDD: red: Movement expects a Player but not found Exception
    
    * TDD: green: Movement now has a Player related and its migrations
    
    * TDD4: green: Movement models covered and completed

--- mastermindAPI/board/tests.py
+++ mastermindAPI/board/tests.py
@@ -3,7 +3,7 @@ import mock
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from board.models import Game, create_code, decode
+from board.models import Game, create_code, decode, Movement
 
 
 class BoardTestCase(TestCase):
@@ -28,6 +28,10 @@ class BoardTestCase(TestCase):
 
     def test_check_combination(self):
         combination = ['R', 'G', 'B', 'P']
+        combination2 = ['R', 'G', 'B', 'Z']
+        game = Game(code = ['R', 'G', 'B', 'P'])
+        self.assertEqual(game.check_combination(combination=combination), True)
+        self.assertFalse(game.check_combination(combination2))
         game = Game(code = ['R', 'G', 'B', 'P'])
         self.assertEqual(game.check_combination(combination=combination), True)
 
@@ -40,9 +44,11 @@ class BoardTestCase(TestCase):
         combination1 = ['A', 'C', 'Z', 'H']
         combination2 = ['R', 'G', 'Y', 'P']
         combination3 = ['R', 'G', 'B', 'P']
+        combination4 = ['A', 'Z', 'C', 'R']
         self.assertEqual(self.game.check_pegs(combination1), (0,0))
         self.assertEqual(self.game.check_pegs(combination2), (0,3))
         self.assertEqual(self.game.check_pegs(combination3), (0,4))
+        self.assertEqual(self.game.check_pegs(combination4), (1,0))
 
     def test_finish_game(self):
         self.game.end_game()
@@ -53,3 +59,27 @@ class BoardTestCase(TestCase):
 
     def test_game_owner(self):
         self.assertEqual(self.game.player.username, self.player.username)
+
+
+class MovementTestCase(TestCase):
+    def setUp(self):
+        self.player = User.objects.create(username="test")
+        self.game = Game.objects.create(player=self.player)
+        self.movement = Movement.objects.create(game=self.game,
+                                                player=self.player)
+
+    def test_create_movement(self):
+        self.assertIsNotNone(self.movement)
+
+    def test_movement_representation(self):
+        m = Movement.objects.create(result="something",
+                                    game=self.game, player=self.player)
+        self.assertEqual("{} - {} - {}".format(m.player, self.game, m.result),
+                         m.__str__())
+
+    def test_movement_game_relation(self):
+        movement = Movement.objects.create(game=self.game, player=self.player)
+        self.assertIsNotNone(movement)
+
+    def test_movement_player(self):
+        self.assertIsNotNone(self.movement.player)
