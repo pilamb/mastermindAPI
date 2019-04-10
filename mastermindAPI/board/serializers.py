6810fe6e01d450c8d58917ad43f03efc193745ae
P.C <pilamb@users.noreply.github.com>
2019-04-10 22:53:37 +0200

    Tdd5 serializers (#10)
    * TDD5: red: preparing files for testing Serializers but import files since they dont exist
    
    * TDD5: green: serializers file created empty but test passes
    
    * TDD5: green: Game serializer first test using it that passes
    
    * TDD5: green: Game serializers tests
    
    * TDD5: red: MovementSerializer does not exist, need to create it
    
    * TDD5: green: MovementSerializer created
    
    * TDD5: green: MovementSerializers tests
    
    * TDD5 Adding related field to Board to show Movements

---
+++ mastermindAPI/board/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Game, Movement
+
+
+
+class MovementSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(source='get_code_display')
+    player = serializers.ReadOnlyField(source='player.username')
+    created = serializers.DateTimeField()
+    game = serializers.HyperlinkedRelatedField(many=False,
+                                               read_only=True,
+                                               view_name='game-detail')
+    result = serializers.CharField()
+    class Meta:
+        model = Movement
+        fields = ('code', 'player', 'created', 'game', 'result')
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(source='get_code_display')
+    player = serializers.ReadOnlyField(source='player.username')
+    finished = serializers.ReadOnlyField()
+    movements = MovementSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Game
+        fields = ('player', 'created', 'code', 'finished', 'movements')
---
+++ mastermindAPI/board/tests/test_serializers.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from django.contrib.auth.models import User as Player
+
+from board.models import Game, Movement
+from board.serializers import BoardSerializer, MovementSerializer
+
+
+class GameSerializersTestCase(TestCase):
+    def setUp(self):
+        self.player = Player.objects.create()
+        self.game = Game.objects.create(player=self.player)
+        self.data = {'code': str(self.game.code), 'player': self.player}
+        self.serializer = BoardSerializer(data=self.data)
+
+    def test_board_serializer(self):
+        serializer = BoardSerializer(instance=self.game)
+        self.assertIsNotNone(serializer.instance)
+
+    def test_board_serializer_is_valid(self):
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_board_serializer_creation(self):
+        serializer = BoardSerializer(data=self.data)
+        serializer.is_valid()
+        self.assertIsNotNone(serializer.data)
+        self.assertEqual(serializer.data.get('code'), str(self.game.code))
+
+    def test_board_has_movements(self):
+        self.assertTrue('movements' in self.serializer.fields.keys())
+
+
+class MovementSerializerTestCase(TestCase):
+    def setUp(self):
+        self.player = Player.objects.create()
+        self.game = Game.objects.create(player=self.player)
+        self.movement = Movement.objects.create(code=str(self.game.code),
+                                                game=self.game,
+                                                player=self.player)
+        self.data = {'game': self.game,
+                     'player': self.player,
+                     'code': str(self.game.code)}
+        self.serializer = MovementSerializer(data=self.data)
+
+    def test_movement_serializer_creation(self):
+        self.assertIsNotNone(self.serializer)
+
+    def test_serializer_usage(self):
+        serializer = MovementSerializer(instance=self.movement)
+        self.assertEqual(serializer.instance, self.movement)
+
+    def test_serializer_is_valid(self):
+        serializer = MovementSerializer(data=self.data)
+        serializer.is_valid()
+        data = serializer.data
+        self.assertIsNotNone(data)
