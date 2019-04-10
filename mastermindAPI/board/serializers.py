b099a0332d37896f79f3dc88d2c2ea916ae4b33b
pilamb <perikopalotes@gmail.com>
2019-04-10 21:58:08 +0200

    TDD5: green: serializers file created empty but test passes

---
+++ mastermindAPI/board/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+from .models import Game, Movement
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    pass
--- mastermindAPI/board/tests/test_serializers.py
+++ mastermindAPI/board/tests/test_serializers.py
@@ -4,7 +4,10 @@ from django.test import TestCase
 from rest_framework.serializers import ValidationError
 
 from board.models import Game, Movement
-from board.serializers import
+from board.serializers import Game
 
 
 class GameSerializersTestCase(TestCase):
+
+    def test_game_serializer(self):
+        pass
