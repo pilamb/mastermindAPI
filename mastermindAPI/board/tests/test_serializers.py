9475f999cff9b73513550ee20521916f2abd4fda
pilamb <perikopalotes@gmail.com>
2019-04-10 22:21:31 +0200

    TDD5: green: Game serializers tests

--- mastermindAPI/board/tests/test_serializers.py
+++ mastermindAPI/board/tests/test_serializers.py
@@ -1,17 +1,27 @@
 from django.test import TestCase
 from django.contrib.auth.models import User as Player
 
-from rest_framework.serializers import ValidationError
-
 from board.models import Game, Movement
 from board.serializers import BoardSerializer
 
 
 class GameSerializersTestCase(TestCase):
 
+    def setUp(self):
+        self.player = Player.objects.create()
+        self.game = Game.objects.create(player=self.player)
+        self.data = {'code': str(self.game.code), 'player': self.player}
+        self.serializer = BoardSerializer(data=self.data)
+
     def test_board_serializer(self):
-        player = Player.objects.create()
-        game = Game.objects.create(player=player)
-        serializer = BoardSerializer(data={'code': str(game.code),
-                                           'player':player})
+        serializer = BoardSerializer(instance=self.game)
         self.assertIsNotNone(serializer.instance)
+
+    def test_board_serializer_is_valid(self):
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_board_serializer_creation(self):
+        serializer = BoardSerializer(data=self.data)
+        serializer.is_valid()
+        self.assertIsNotNone(serializer.data)
+        self.assertEqual(serializer.data.get('code'), str(self.game.code))
