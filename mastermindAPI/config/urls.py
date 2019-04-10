3437aa7dea8c98708cec80fb6aa462d00bf16268
pilamb <perikopalotes@gmail.com>
2019-04-10 23:07:45 +0200

    View for Movements and its url

--- mastermindAPI/config/urls.py
+++ mastermindAPI/config/urls.py
@@ -2,9 +2,12 @@ from django.urls import include, path
 from rest_framework import routers
 from players.views import UserViewSet
 
+from board.views import MovementView
+
 
 router = routers.DefaultRouter()
 router.register(r'players', UserViewSet)
+router.register(r'movements', MovementView)
 
 urlpatterns = [
     path('', include(router.urls)),
