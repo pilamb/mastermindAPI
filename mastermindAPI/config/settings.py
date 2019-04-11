72ea26cf1ca88194737b333aa1deae73b8a4ba76
P.C <pilamb@users.noreply.github.com>
2019-04-11 16:11:20 +0200

    Develop (#18)
    * forgotten idea folder
    
    * Tdd5 serializers ii (#12)
    
    * View for Movements and its url
    
    * Game View, Url and some comments on READme
    
    * Adding Action Views to create a Game with a valid loged in User, some links into README
    
    * When a user creates a game it get registered into Movements table to make it traceable
    
    * Feature play game (#13)
    
    * Playing enabled. Now a user can enter a code and receive black and white pegs feedback or if won or loose the game. Utils is a module to validate user input, and transform to low-level colors
    
    * PEP8 cleaned code
    
    * Feature play game (#15)
    
    * Playing enabled. Now a user can enter a code and receive black and white pegs feedback or if won or loose the game. Utils is a module to validate user input, and transform to low-level colors
    
    * PEP8 cleaned code
    
    * Setting refactor; number of turns and number of pegs per turn are customizable and dinamic thought settings
    
    * Refactor - hotfix to handle both String and List unpacking when checking pegs
    
    * Utils tests and refactor with a bug found
    
    * Credentials are mandatory now, or readOnly (#16)

--- mastermindAPI/config/settings.py
+++ mastermindAPI/config/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 # GAME SETTINGS
