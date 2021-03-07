a55b77a95247c2cde4a552f1a7f9836bd6ac70dc
pilamb <perikopalotes@gmail.com>
2021-03-07 14:18:06 +0100

    Vue.js PoC

--- mastermindAPI/config/urls.py
+++ mastermindAPI/config/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
+
+from board.views import GameView, index, MovementView
 from players.views import UserViewSet
 
-from board.views import GameView, MovementView
 
 
 router = routers.DefaultRouter()
@@ -12,5 +15,7 @@ router.register(r'movements', MovementView)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('home', index, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
