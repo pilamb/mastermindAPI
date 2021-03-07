fe278de1738d292bea90b36e2579168a4736ab76
pilamb <perikopalotes@gmail.com>
2021-03-07 21:54:40 +0100

    some more examples

--- mastermindAPI/front/src/mastermind.js
+++ mastermindAPI/front/src/mastermind.js
@@ -2,13 +2,33 @@ var app = new Vue({
   el: '#app',
   delimiters: ['[[', ']]'],
   data: {
-    message: 'Hola Vue!'
+    message: 'Hello Vueworld!'
   }
 })
 
 var app2 = new Vue({
   el: '#app-2',
   data: {
-    message: 'Usted cargó esta página el ' + new Date().toLocaleString()
+    message: 'Page loaded at' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  delimiters: ['[[', ']]'],
+  data: {
+    todos: [
+      { text: 'Icons for colors png' },
+      { text: 'Add Bootstrap' },
+      { text: 'board design' },
+      { text: 'Add header with current user' },
+    ]
   }
 })
